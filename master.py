a52e369a2bb341511535eb2b9fe99c00fa55e6f2
SergeyChaliy <70662544+sergeychaliy2@users.noreply.github.com>
2023-10-25 21:04:01 +0300

    Update master.py

--- master.py
+++ master.py
@@ -1,63 +1,15 @@
-import requests
-from bs4 import BeautifulSoup
-import re
-import pymorphy2
-
-morph = pymorphy2.MorphAnalyzer()
-
-# slave_1
-def count_words_on_webpage(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, 'html.parser')
-        words = re.findall(r'\b\w+\b', soup.get_text(), re.IGNORECASE)
-        return len(words)
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
-
-# slave_2
-def count_specific_words_on_webpage(url, specific_words):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, 'html.parser')
-        text = soup.get_text()
-        word_count = {}
-
-        for word in specific_words:
-            normalized_word = morph.parse(word)[0].normal_form
-            count = len(re.findall(rf'\b{normalized_word}\b', text, re.IGNORECASE))
-            if count > 0:
-                word_count[normalized_word] = count
-
-        return word_count
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
-
-# indetive word
-def find_similar_words(word):
-    parsed_word = morph.parse(word)[0]
-    return [parsed_word.normal_form] + [inflection.word for inflection in parsed_word.lexeme]
-
-# master
-def word_count_wizard():
-    url = input("Enter the URL: ")
-    specific_words = input("Enter words: ").split(',')
-    word_count = count_words_on_webpage(url)
-    specific_word_count = count_specific_words_on_webpage(url, specific_words)
-
-    if word_count is not None:
-        print(f"Word count: {word_count}")
-
-    if specific_word_count is not None:
-        print("Word count:")
-        for word, count in specific_word_count.items():
-            print(f"'{word}': {count}")
-            similar_words = find_similar_words(word)
-            print(f"words: {', '.join(similar_words)}")
+import multiprocessing
+import os
 
 if __name__ == "__main__":
-    word_count_wizard()
+    url = input("Enter the URL: ")
+    specific_words = input("Enter words: ").split(',')
+
+    # Запуск слейвов параллельно
+    with multiprocessing.Pool(processes=2) as pool:
+        result1 = pool.apply_async(os.system, ("python slave1.py",))
+        result2 = pool.apply_async(os.system, ("python slave2.py",))
+
+    # Получение результатов
+    result1.get()
+    result2.get()
